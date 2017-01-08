e5e2d8dfeb990589c7f1078123fc8d78be358919
srendi@gmail.com <srendi@gmail.com>
2017-01-09 09:25:53 +1000

    Added logger -s (syslog) entries

--- lumiBash.sh
+++ lumiBash.sh
@@ -20,10 +20,11 @@ install_salt() {
 	sudo add-apt-repository -y ppa:saltstack/salt
 	sudo apt-get update
 	sudo apt-get install -y salt-master salt-minion salt-ssh salt-cloud salt-doc
+	logger -s "Gavin: Salt installled"
 
 	# Open firewall
 	sudo ufw allow salt
-
+	logger -s "Gavin: Salt Firewall opened"
 	# Installed. Now restart servces
 	sudo stop salt-minion
 	sudo stop salt-master
@@ -31,15 +32,17 @@ install_salt() {
 	sudo start salt-master
 	sudo start salt-minion
 	sleep 3
-
+	logger -s "Gavin: Salt Started"
 	# Setup minion keys
 	sudo salt-key --list all
 	sudo salt-call key.finger --local
 	sudo salt-key -y -A
 	sleep 3
+	logger -s "Gavin: Salt Minion key accepted"
 }
 
 install_packages() {
+	logger -s "Gavin: Installing Packages"
 	# Ubuntu12.04 default apt-get bug
 	sudo rm -rf /var/lib/apt/lists/*
 	sudo apt-get update
@@ -52,6 +55,7 @@ install_packages() {
 	#apt-get -y upgrade
 	sudo apt-get install -y git
 	sudo apt-get install -y msgpack-python python-crypto
+	logger -s "Gavin: Packages Installed"
 	# Config directory for salt
 	sudo mkdir -p /srv/salt
 	sudo mkdir -p /srv/pillar
@@ -64,37 +68,46 @@ install_packages() {
 	sudo git clone https://github.com/Srendi/LumiDeployFlask.git
 	cd /srv/salt/LumiDeployFlask/
 	sudo git pull
+	logger -s "Gavin: LumiDeployFlask Installed"
 	sudo mkdir -p /etc/salt/master.d/
 	sudo mkdir -p /etc/salt/minion.d/
 	sudo cp /srv/salt/LumiDeployFlask/salt/files/etc/salt/minion.d/minion.conf /etc/salt/minion.d/
 	sudo cp /srv/salt/LumiDeployFlask/salt/files/etc/salt/master.d/master.conf /etc/salt/master.d/
 	sudo mkdir -p /etc/nginx/conf.d/
 	sudo cp /srv/salt/LumiDeployFlask/nginx/files/etc/nginx/conf.d/nginx.conf /etc/nginx/conf.d/
+	logger -s "Gavin: Package Directories Created"
 }
 
 run_highstate() {
+logger -s "Gavin: Restarting Salt Prior to calling highstate"
 	sudo stop salt-minion
 	sudo stop salt-master
 	sleep 3
 	sudo start salt-master
 	sudo start salt-minion
 	sleep 10
+	logger -s "Gavin: Calling Salt highstate"
 	echo "Calling salt highstate"
 	cd /srv/salt/LumiDeployFlask/
 	sudo salt '*' state.apply
 	sleep 3
+	logger -s "Gavin: Salt highstate applied"
 }
 
 deploy_app() {
+	logger -s "Gavin: Deploying App"
 	cd /var/www/
 	sudo git clone https://github.com/Srendi/LumiFlaskBlog.git
 	cd /var/www/LumiFlaskBlog/
 	sudo git pull
+	logger -s "Gavin: App Deployed"
 }
 
 start_app() {
+	logger -s "Gavin: Starting gunicorn"
 	cd /var/www/LumiFlaskBlog
 	sudo gunicorn -w 4 -b 127.0.0.1:5000 $usedApp &
+	logger -s "Gavin: gunicorn started"
 }
 
 #Main
