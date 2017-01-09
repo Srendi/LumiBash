5d112acb39b685c628b62e2fdb7a90659d187aa5
srendi@gmail.com <srendi@gmail.com>
2017-01-09 15:50:35 +1000

    no sudo for gunicorn

--- lumiBash.sh
+++ lumiBash.sh
@@ -126,7 +126,7 @@ deploy_app() {
 start_app() {
 	logger -s "Gavin: Starting gunicorn"
 	cd /var/www/LumiFlaskBlog/
-	sudo gunicorn -w 4 -b 127.0.0.1:5000 hello:app
+	gunicorn -w 4 -b 127.0.0.1:5000 hello:app
 	logger -s "Gavin: gunicorn started"
 }
 
